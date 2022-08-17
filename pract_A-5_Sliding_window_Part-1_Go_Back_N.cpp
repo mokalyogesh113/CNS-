8ef8c46023a84f28d005c84407fb64abaf4a40a6
Yogesh Mokal <mokalyogesh113@gmail.com>
2022-08-17 22:43:24 +0530

    Update pract_A-5_Sliding_window_Part-1_Go_Back_N.cpp

--- pract_A-5_Sliding_window_Part-1_Go_Back_N.cpp
+++ pract_A-5_Sliding_window_Part-1_Go_Back_N.cpp
@@ -3,23 +3,27 @@ using namespace std;
 
 int main()
 {
+    // Input the size of the sliding window
     int w;
-    cout << "Enter the size of the sliding window :- ";
+    cout << "\nEnter the size of the sliding window :- ";
     cin >> w;
 
+    // Input the number of frames and all frames
     int f;
-    cout << "Enter the number of frames :- ";
+    cout << "\nEnter the number of frames :- ";
     cin >> f;
 
     int frames[f];
-    cout << "Enter the " << f << " Frames one by one :- \n";
+    cout << "\nEnter the " << f << " Frames one by one :- \n";
     for (int i = 0; i < f; i++)
     {
         cin >> frames[i];
     }
 
-    cout << "\n\n(1 - Send acknowledgement 0-do not send acknowledgement) " << endl;
+    // To send Acknowledgement press '1' and press '0' not to send Acknowledgement
+    cout << "\n\n(1 - Send Acknowledgement 0-do not send Acknowledgement) " << endl;
 
+    // Sending The Frames of Initial Sliding Window
     for (int i = 0; i < w; i++)
     {
         if (i < f)
@@ -28,18 +32,23 @@ int main()
         }
     }
 
+    /*
+        Take Acknowledgement of each frame
+        If Acknowledgement received then send next one single frame  &
+        If Acknowledgement not received then send whole sliding window again from starting
+    */
     bool flag = 0;
     for (int i = 0; i < f; i++)
     {
 
-        cout << "\nSend the acknowledgement of '" << frames[i] << "' :- ";
+        cout << "\nSend the Acknowledgement of '" << frames[i] << "'\n (1 or 0) :- ";
         cin >> flag;
 
         if (flag == 1)
         {
             if (i + w < f)
             {
-                cout << "Sending data " << frames[i + w] << "..." << endl;
+                cout << "\nSending data " << frames[i + w] << "..." << endl;
             }
         }
         else
@@ -48,12 +57,13 @@ int main()
             {
                 if (j < f)
                 {
-                    cout << "Sending data " << frames[j] << "..." << endl;
+                    cout << "\nSending data " << frames[j] << "...";
                 }
             }
+            cout << endl;
             i--;
         }
     }
 
-    cout << "\n\n Data Transfer Statistics Successfully "<<endl;
+    cout << "\n\n Frames Transfered Successfully " << endl;
 }
